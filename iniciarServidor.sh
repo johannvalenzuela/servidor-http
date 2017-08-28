43783792d621633a33d7e6b6aee0528bf699479d
Johann <johann.aevt@gmail.com>
2017-08-27 21:38:45 -0300

    Archivo Bash funcionando

--- iniciarServidor.sh
+++ iniciarServidor.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-python servidor_demo.py
+node server
--- server.js
+++ server.js
@@ -7,16 +7,16 @@ const ROOT = 'documentRoot';
 http.createServer(function (req, res) {
     var q = url.parse(req.url, true);
     var filename = q.pathname;
-    fs.readFile(ROOT+filename, function(err, data) {
-      if (err) {
-        res.writeHead(404, {'Content-Type': 'text/html'});
-        console.log(ROOT+filename);
-        res.writeHead(404, {'X-ResponseEcho': JSON.stringify(req.headers)});
-        return res.end("404 Not Found");
-      }
-      res.writeHead(200, {'X-ResponseEcho': JSON.stringify(req.headers)});
-      res.writeHead(200, {'Content-Type': 'text/html'});
-      res.write(data);
-      return res.end();
+    fs.readFile(ROOT + filename, function (err, data) {
+        if (err) {
+            res.writeHead(404, { 'Content-Type': 'text/html' });
+            console.log(ROOT + filename);
+            res.writeHead(404, { 'X-ResponseEcho': JSON.stringify(req.headers) });
+            return res.end("404 Not Found");
+        }
+        res.writeHead(200, { 'X-ResponseEcho': JSON.stringify(req.headers) });
+        res.writeHead(200, { 'Content-Type': 'text/html' });
+        res.write(data);
+        return res.end();
     });
 }).listen(9100);
