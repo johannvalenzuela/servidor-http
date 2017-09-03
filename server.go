d2e5ca2451515a0e4aece0026edc03be15a8977c
Johann <johann.aevt@gmail.com>
2017-09-02 22:31:11 -0300

    Go

--- server.go
+++ server.go
@@ -3,7 +3,6 @@ package main
 import (
 	"fmt"
 	"net/http"
-	"strings"
 	"log"
 )
 
@@ -12,14 +11,16 @@ func test(res http.ResponseWriter, req *http.Request){
 	//fmt.Println(req.Form)
 	//fmt.Println("path", req.URL.Path)
 	//fmt.Println("scheme", req.URL.Scheme)
-	for k, v := range req.Form {
-		fmt.Println("key:", k)
-		fmt.Println("val:", strings.Join(v,""))
-	}
+	//for k, v := range req.Form {
+		//fmt.Println("key:", k)
+		//fmt.Println("val:", strings.Join(v,""))
+	//}
+	staticDir := "/documentRoot"
+	file := staticDir + req.URL.Path
+	http.ServeFile(res, req, file)
 	
-	res.Header().Set("Content-Type", "text/html")
-	fmt.Println("/documentRoot/" + req.URL.Path[1:])
-	http.ServeFile(res , req, "/documentRoot/" + req.URL.Path[1:])
+	fmt.Println("/documentRoot" + req.URL.Path)
+	
 	
 	//fmt.Fprintf(res,"HOLAAAAA")
 }
